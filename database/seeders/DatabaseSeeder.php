3a8b3e3818e5c22f03ce159b7fddd2d55d9ffbc3
LAURITE <missmiavalentino@gmail.com>
2022-06-28 22:05:44 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,27 +17,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(){
     DB::statement('SET FOREIGN_KEY_CHECKS=0;');
- $category = Category::create([
- 'category_name' => 'Salāti',
- ]);
- $category = Category::create([
-    'category_name' => 'Zupas',
-    ]);
-    $category = Category::create([
-        'category_name' => 'Saldie',
-        ]);
- $category = Category::where('category_name', 'Salāti')->first();
- $category->recipes()->create(['recipe_name' => 'Cēzara salāti']);
- $recipe = new Recipe();
- $recipe->recipe_name = 'Aukstā zupa';
- $category = Category::where('category_name', 'Zupas')->first();
- $category->recipes()->save($recipe);
+//  $category = Category::create([
+//  'category_name' => 'Salāti',
+//  ]);
+//  $category = Category::create([
+//     'category_name' => 'Zupas',
+//     ]);
+//     $category = Category::create([
+//         'category_name' => 'Saldie',
+//         ]);
+//  $category = Category::where('category_name', 'Salāti')->first();
+//  $category->recipes()->create(['recipe_name' => 'Cēzara salāti']);
+//  $recipe = new Recipe();
+//  $recipe->recipe_name = 'Aukstā zupa';
+//  $category = Category::where('category_name', 'Zupas')->first();
+//  $category->recipes()->save($recipe);
 
- $category = Category::where('category_name', 'Saldie')->first();
- $recipe = new Recipe();
- $recipe->recipe_name = 'Ābolmaize';
- $recipe->category()->associate($category);
- $recipe->save();
+//  $category = Category::where('category_name', 'Saldie')->first();
+//  $recipe = new Recipe();
+//  $recipe->recipe_name = 'Ābolmaize';
+//  $recipe->category()->associate($category);
+//  $recipe->save();
 
  DB::statement('SET FOREIGN_KEY_CHECKS=1;');
  }
